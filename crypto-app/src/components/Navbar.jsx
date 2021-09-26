ff0b827c61aff0ad13713f5c1044eceef1bafa70
Jorch <jorch.rl@gmail.com>
2021-09-25 19:06:12 -0500

    Add Layout structure and Homepage component

---
+++ crypto-app/src/components/Navbar.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import {
+    MenuOutlined,
+    FundOutlined,
+    BulbOutlined,
+    HomeOutlined,
+    MoneyCollectOutlined,
+} from "@ant-design/icons";
+
+import { Link } from "react-router-dom";
+import { Button, Menu, Typography, Avatar } from "antd";
+
+import icon from "../images/cryptocurrency.png";
+
+const Navbar = () => {
+    return (
+        <div className="nav-container">
+            <div className="logo-container">
+                <Avatar src={icon} size="large" />
+                <Typography.Title level={2} className="logo">
+                    <Link to="/">Cryptoverse</Link>
+                </Typography.Title>
+                {/* <Button className='menu-control-container'></Button> */}
+            </div>
+            <Menu theme="dark">
+                <Menu.Item icon={<HomeOutlined />}>
+                    <Link to="/">Home</Link>
+                </Menu.Item>
+                <Menu.Item icon={<FundOutlined />}>
+                    <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+                </Menu.Item>
+                <Menu.Item icon={<MoneyCollectOutlined />}>
+                    <Link to="/exchanges">Exchanges</Link>
+                </Menu.Item>
+                <Menu.Item icon={<HomeOutlined />}>
+                    <Link to="/news">News</Link>
+                </Menu.Item>
+            </Menu>
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ crypto-app/src/components/index.js
@@ -0,0 +1,10 @@
+/**
+ * Export all of our components from a single file
+ * to avoid clutter on our App file
+ */
+export { default as Navbar } from "./Navbar";
+export { default as Homepage } from "./Homepage";
+export { default as News } from "./News";
+export { default as Exchanges } from "./Exchanges";
+export { default as CryptoDetails } from "./CryptoDetails";
+export { default as Cryptocurrencies } from "./Cryptocurrencies";
