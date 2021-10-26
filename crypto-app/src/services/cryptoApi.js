036537ecb2924dd2f953fe740ae7692184f07c41
Jorch <jorch.rl@gmail.com>
2021-10-26 16:14:26 -0500

    Add redux store and Api for cryptocurrency info

---
+++ crypto-app/src/services/cryptoApi.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const baseUrl = "https://coinrankin1.p.rapidapi.com";
+const cryptoApiHeaders = {
+  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+  "x-rapidapi-key": "c4314d37fbmshf97f38cb38d2e81p1df0dfjsnaffc99eadd9c", //my key?
+};
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest("/coins"),
+    }),
+  }),
+});
+
+export const { useGetCryptosQuery } = cryptoApi;
